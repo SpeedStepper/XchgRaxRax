eb0c8f3f61c3330af310820be7e7e8cce620d3b7
Vince <vincenzocallocchia@icloud.com>
2021-11-02 15:11:16 -0400

    0x01.asm correction
    Updated 0x01 and fixed the error of not initializing %rdx and %rcx registers.
    
    Added assembler formatting.

--- 0x01.asm
+++ 0x01.asm
@@ -1,45 +1,18 @@
-;
-; $Id: 0x01.asm,v 1.1.1.1 2016/03/27 08:40:12 raptor Exp $
-;
-; 0x01 explanation - from xchg rax,rax by xorpd@xorpd.net
-; Copyright (c) 2016 Marco Ivaldi <raptor@0xdeadbeef.info>
-;
-; This snippet demonstrates an elegant way to generate the
+; This snippet, 0x01 demonstrates an elegant way to generate the
 ; Fibonacci Sequence: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
-;
-; Example:
-; uncomment the added lines
-; $ nasm -f elf64 0x01.asm
-; $ gcc 0x01.o -o 0x01
-; $ gdb 0x01
-; (gdb) b main.loop
-; (gdb) r
-; (gdb) i r rax
-; rax            0x0	0
-; (gdb) c
-; (gdb) i r rax
-; rax            0x1	1
-; (gdb) c
-; (gdb) i r rax
-; rax            0x1	1
-; (gdb) c
-; (gdb) i r rax
-; rax            0x2	2
-; (gdb) c
-; (gdb) i r rax
-; rax            0x3	3
-; (gdb) c
-; (gdb) i r rax
-; rax            0x5	5
-;
+; Assemble with $ nasm -felf64 0x01.asm && ld 0x01.o && ./a.out
 
-	BITS 64
-	SECTION .text
-	global main
+	global	_start
 
-main:
-	;mov	rax,0		; initialize the rax register
-	;mov	rdx,1 		; initialize the rdx register
-.loop:
-	xadd	rax,rdx
-	loop	.loop
+	section	.text
+_start:
+	mov 	rcx, loop_iteration	; Initialize the rcx register.
+	mov		rax, 1				; Initialize the rax register.
+	mov		rdx, 1 				; Initialize the rdx register.
+
+fib_loop:
+	xadd	rax, rdx				; Exchange and add.
+	loop	fib_loop
+
+	section	.data
+loop_iteration equ 10			; Arbitrary iteration number.
