845d9c4b1789487f79abaf629ff9cf3e9161006d
Vince <vincenzocallocchia@icloud.com>
2021-11-02 14:48:00 -0400

    Update 0x00.asm
    Added assembler formatting and extra info.

--- 0x00.asm
+++ 0x00.asm
@@ -1,40 +1,20 @@
-;
-; $Id: 0x00.asm,v 1.1.1.1 2016/03/27 08:40:12 raptor Exp $
-;
-; 0x00 explanation - from xchg rax,rax by xorpd@xorpd.net
-; Copyright (c) 2016 Marco Ivaldi <raptor@0xdeadbeef.info>
-;
-; This is the snippet #0: it sets registers to 0 in different ways.
-;
-; Example:
-; uncomment the added lines
-; $ nasm -f elf64 0x00.asm
-; $ gcc 0x00.o -o 0x00
-; $ gdb 0x00
-; (gdb) b debug
-; (gdb) r
-; (gdb) i r
-; rax            0x0	0
-; rbx            0x0	0
-; rcx            0x0	0
-; rdx            0x0	0
-; rsi            0x0	0
-; rdi            0x0	0
-; rbp            0x0	0x0
-; [...]
-;
+; This snippet, 0x00, sets all registers to 0 in different ways.
+; Assemble with $ nasm -felf64 0x00.asm && ld 0x00.o && ./a.out
 
-	BITS 64
-	SECTION .text
-	global main
+	global	_start
 
-main:
-	xor	eax,eax		; set rax to 0 by xor'ing it with itself
-	lea	rbx,[0]		; set rbx to 0 by loading the value 0 into it
-	;mov	ecx,10		; added to make the following loop faster
-	loop	$		; set rcx to 0 by decrementing it via loop
-	mov	rdx,0 		; set rdx to 0 using the mov instruction
-	and	esi,0		; set rsi to 0 by and'ing it with 0
-	sub	edi,edi		; set rdi to 0 by subtracting its current value
+	section	.text
+_start:
+	xor		eax, eax		; Set rax to 0 by xor'ing it with itself.
+	lea		rbx, [0]			; Set rbx to 0 by loading the value 0 into it.
+	mov		ecx, loop_iteration
+	loop	$				; Set rcx to 0 by decrementing it via a loop.
+	mov		rdx, 0 			; Set rdx to 0 using the mov instruction.
+	and		esi, 0			; Set rsi to 0 by and'ing it with 0.
+	sub		edi, edi		; Set rdi to 0 by subtracting its current value.
 	push	0
-	pop	rbp		; set rbp to 0 using push and pop instructions
+	pop		rbp				; Set rbp to 0 using push and pop instructions.
+
+	section	.data
+
+loop_iteration	equ	10
