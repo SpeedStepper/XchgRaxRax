d0b8f082a9c32af17637da7531d3db0ef812ada0
Vince <vincenzocallocchia@icloud.com>
2021-11-04 13:39:33 -0400

    Update 0x04 for simple explanation

--- 0x03.asm
+++ 0x03.asm
@@ -6,13 +6,13 @@
 	
 	section	.text
 _start:
-	mov	rax,1		; initialize the rax register
-	mov	rdx,2		; initialize the rdx register
+	mov		rax, 1		; initialize the rax register
+	mov		rdx, 2		; initialize the rdx register
 	
-	sub	rdx,rax		; rdx = rdx - rax; cf = 1 if rdx < rax
-	sbb	rcx,rcx		; rcx = rcx - rcx - cf (either 0 or -1)
-	and	rcx,rdx		; rcx = (rdx - rax) if (cf); rcx = 0 if (!cf)
-	add	rax,rcx		; rax = rax + (rdx - rax) if (cf); rax if (!cf)
+	sub		rdx, rax		; rdx = rdx - rax; cf = 1 if rdx < rax
+	sbb		rcx, rcx		; rcx = rcx - rcx - cf (either 0 or -1)
+	and		rcx, rdx		; rcx = (rdx - rax) if (cf); rcx = 0 if (!cf)
+	add		rax, rcx		; rax = rax + (rdx - rax) if (cf); rax if (!cf)
 
 ; Appears to be a conditional assignment that doesn’t use an actual conditional instruction. In pseudo code, it would like thing like this.
 ;if (rdx < rax) then
