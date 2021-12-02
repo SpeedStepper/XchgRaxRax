4978b461ac9f548ddb9263753856fbead5541232
Vince <vincenzocallocchia@icloud.com>
2021-12-02 16:37:52 -0500

    Update 0x09.asm

--- 0x09.asm
+++ 0x09.asm
@@ -7,9 +7,13 @@
 	
 	section	.text
 _start:
+	mov		rax, 1		; Initialize rax (any value).
+
 	shr		rax, 3		; rax = (int)rax / 8, cf = 1 if 3rd bit is 1
 	adc		rax, 0		; rax = rax + cf to round up.
 
 ; In other words, (int)(rax + 4) / 8 is computed and the result
 ; is rounded to the nearest integer.
+
+; Recall that dividing an unsigned number by 8 is the same as shifting the number 3 times to the right.
 
